38961630753cfde45d984b5230b87e3e7cde3de2
myazhbin <myazhbin@gmail.com>
2015-10-21 03:19:24 +0800

    first commit to github

---
+++ transpiler/lexer/lexer.js
@@ -0,0 +1,120 @@
+var lexerFunctions = require("./lexerFunctions");
+
+module.exports = function(code) {
+  var NUMBER = /^0b[01]+|^0o[0-7]+|^0x[\da-f]+|^\d*\.?\d+(?:e[+-]?\d+)?/i;
+  
+  code = code.trim();
+  var i = 0;
+  var tokens = [];
+  var chunk = '';
+  var currCol, prevCol, nextCol;
+  var VARIABLE_NAMES = {};
+  
+  // track state
+  var insideString = {status: false};
+  var insideNumber = {status: false};
+  var insideCollection = [];
+  var stringInterpolation = {status: false, counter: 0};
+  var substringLookup = {status: false};
+  // TODO - scope
+
+  // advances the position of i by specified number of positions
+  var advance = function(positions) {
+    i += positions;
+  };
+
+  var clearChunk = function() {
+    chunk = '';
+  };
+
+  // advances the position of i by specified number of positions and clears chunk
+  var advanceAndClear = function(positions) {
+    i += positions;
+    clearChunk();
+  };
+
+  while (code[i] !== undefined) {
+    debugger;
+    chunk += code[i];
+    currCol = code[i];
+    prevCol = code[i - 1];
+    nextCol = code[i + 1];
+    var lastCollectionIndex = insideCollection.length - 1;
+    
+    if (currCol === '"' && insideString.status) {
+      insideString.status = false;
+    } else if (currCol === '"') {
+      insideString.status = true;
+    }
+    
+    if (NUMBER.test(chunk) && !insideString.status && !insideNumber.status) {
+      insideNumber.status = true;
+    }
+    if (insideNumber.status && isNaN(nextCol) && nextCol !== '.') {
+      insideNumber.status = false;
+      lexerFunctions.checkForLiteral(chunk, tokens);
+      advanceAndClear(1);
+      lexerFunctions.handleEndOfFile(nextCol, tokens);
+      continue;
+    }
+
+    if (!stringInterpolation.status && nextCol === '\\' && code[i + 2] === '(') {
+      stringInterpolation.status = true;
+      if (chunk !== "") {
+        lexerFunctions.checkForLiteral(chunk + '"', tokens);
+      }
+      lexerFunctions.makeToken("SPECIAL_STRING", "\\(", tokens);
+      advanceAndClear(3);
+      insideString.status = false;
+      continue;
+    }
+    if (stringInterpolation.status && currCol === ")" && stringInterpolation.counter === 0) {
+      stringInterpolation.status = false;
+      lexerFunctions.makeToken("SPECIAL_STRING", ")", tokens);
+      advanceAndClear(1);
+      chunk = '"';
+      insideString.status = true;
+      continue;
+    }
+    
+    console.log(chunk);
+    if (!insideString.status && !insideNumber.status && 
+      lexerFunctions.checkForEvaluationPoint(currCol, nextCol)) {
+
+      if (insideCollection.length && 
+        insideCollection[lastCollectionIndex]['type'] === undefined &&
+        lexerFunctions.checkFor('PUNCTUATION', chunk, tokens)){
+        lexerFunctions.determineCollectionType(insideCollection, tokens);
+      } else if (insideCollection.length && currCol === ']' && !substringLookup.status) {
+        lexerFunctions.checkFor('COLLECTION', chunk, tokens, function() {
+          tokens[tokens.length - 1].type = insideCollection[lastCollectionIndex]['type'] || 'ARRAY_END';
+          insideCollection.pop();
+        });
+      } else if (tokens.length && tokens[tokens.length - 1].type !== 'IDENTIFIER' && currCol === '[') {
+        lexerFunctions.checkFor('COLLECTION', chunk, tokens, function(){
+          insideCollection.push({type: undefined, location: tokens.length-1});})
+      } else {
+        lexerFunctions.checkFor('KEYWORD', chunk, tokens) ||
+        lexerFunctions.checkForIdentifier(chunk, tokens, VARIABLE_NAMES) ||
+        lexerFunctions.checkFor('PUNCTUATION', chunk, tokens) || 
+        lexerFunctions.checkFor('SUBSTRING_LOOKUP', chunk, tokens, function() {
+          substringLookup.status = !substringLookup.status;
+        }) ||
+        lexerFunctions.checkFor('OPERATOR', chunk, tokens) || 
+        lexerFunctions.checkForLiteral(chunk, tokens);
+      }
+      
+      clearChunk();
+      
+      // handles special evaluation point scenarios
+      if (lexerFunctions.checkForWhitespace(nextCol)) advance(1);
+      lexerFunctions.handleEndOfFile(nextCol, tokens);
+      
+    }
+    advance(1);
+    // console.log(tokens);
+  }
+  // console.log(tokens);
+  return tokens;
+  
+};
---
+++ transpiler/lexer/lexerFunctions.js
@@ -0,0 +1,118 @@
+var lexicalTypes = require("./lexicalTypes");
+
+
+
+module.exports = {
+
+  checkForEvaluationPoint: function(currCol, nextCol) {
+    if (
+      
+        module.exports.checkForWhitespace(currCol) ||
+        module.exports.checkForWhitespace(nextCol) || 
+        module.exports.checkFor('PUNCTUATION', nextCol) || 
+        module.exports.checkFor('PUNCTUATION', currCol) || 
+        module.exports.checkFor('OPERATOR', nextCol) || 
+        module.exports.checkFor('OPERATOR', currCol) || 
+        nextCol === '"' || nextCol === ']' || currCol === '[' ||
+        currCol === ']' || nextCol === '[' || nextCol === undefined) {
+      
+      return true;
+    
+    }
+    return false;
+  },
+  
+  makeToken: function(lexicalType, snippet, tokens, type, value) {
+    if (tokens) {
+      var obj = {};
+      obj['type'] = type || lexicalTypes[lexicalType][snippet];
+      obj['value'] = value || snippet.trim();
+      tokens.push(obj);
+    }
+  },
+
+  checkForLiteral: function(snippet, tokens, cb) {
+    snippet = JSON.parse(snippet.trim());
+    var type = typeof snippet;
+    var obj = {
+    'boolean': function(snippet, tokens) {
+                if (tokens) {
+                  module.exports.makeToken(undefined, snippet, tokens, 'BOOLEAN', JSON.stringify(snippet));
+                }
+                if (cb) {cb(snippet, tokens)};
+                return true;
+              },
+    'string': function(snippet, tokens) {
+                if (tokens) {
+                  module.exports.makeToken(undefined, snippet, tokens, 'STRING', snippet);
+                }
+                if (cb) {cb(snippet, tokens)};
+                return true;
+              },
+    'number': function(snippet, tokens) {
+                if (tokens) {
+                  module.exports.makeToken(undefined, snippet, tokens, 'NUMBER', JSON.stringify(snippet));
+                }
+                if (cb) {cb(snippet, tokens)};
+                return true;         
+              }    
+    };
+    if (obj[type] === undefined) {
+      return false;
+    } else {
+      obj[type](snippet, tokens);
+    }
+  },
+  
+  checkFor: function(lexicalType, snippet, tokens, cb) {
+    if(lexicalTypes[lexicalType][snippet]){
+      if (tokens) {
+        module.exports.makeToken(lexicalType, snippet, tokens);
+        if (cb) {
+          cb();
+        }
+      }
+      return true;
+    }
+    return false;
+  },
+  
+  // helper function to check for whitespace
+  checkForWhitespace: function(snippet) {
+    return snippet === ' ';
+  },
+
+  // helper function to check for identifiers
+  checkForIdentifier: function(snippet, tokens, variable_names) {
+      if (variable_names[snippet]) {
+        if (tokens) {
+          module.exports.makeToken(undefined, snippet, tokens, 'IDENTIFIER', snippet);
+        }
+        return true;
+      } else if (tokens[tokens.length - 1].type === 'DECLARATION_KEYWORD') {
+        if (tokens) {
+          module.exports.makeToken(undefined, snippet, tokens, 'IDENTIFIER', snippet);
+        }
+        variable_names[snippet] = true;
+        return true;
+      }
+    return false;
+  },
+  
+  determineCollectionType: function(collectionInformation, tokens, cb) {
+    if (tokens[tokens.length - 1]['value'] === ':') {
+      tokens[collectionInformation[collectionInformation.length - 1]['location']]['type'] = 'DICTIONARY_START';
+      collectionInformation[collectionInformation.length - 1]['type'] = 'DICTIONARY_END';
+    } else {
+      collectionInformation[collectionInformation.length - 1]['type']  = 'ARRAY_END';
+    }
+    if (cb) {
+      cb();
+    }
+  },
+  
+  handleEndOfFile: function(nextCol, tokens) {
+      if (nextCol === undefined) module.exports.makeToken(undefined, undefined, tokens, 'TERMINATOR', 'EOF');
+    }
+  
+};
