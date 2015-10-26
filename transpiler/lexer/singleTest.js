8c79fde7f3b46f340ed6d97389db1d12d249ee89
Dnld <steinert.donald@gmail.com>
2015-10-26 15:46:15 -0400

    updates single test

--- transpiler/lexer/singleTest.js
+++ transpiler/lexer/singleTest.js
@@ -3,33 +3,49 @@ var deepEqual = require("./helperFunctions").deepEqual;
 var diff = require("./helperFunctions").diff;
 
 
-var swiftCode = String.raw`var sum = 0
-                      for i in 0..<5 {
-                          sum += i
-                      }`;
+var swiftCode = String.raw`func greet(name: String, day: String) -> String {
+                        return "Hello \(name), today is \(day)."
+                    }
+                    greet("Bob", day: "Tuesday")`;
      
 var swiftCodeAnswers = [
-    { type: 'DECLARATION_KEYWORD',            value: 'var' },
-    { type: 'IDENTIFIER',                     value: 'sum' },
-    { type: 'OPERATOR',                       value: '=' },
-    { type: 'NUMBER',                         value: '0' },
-    { type: 'TERMINATOR',                     value: '\\n' },
-    { type: "STATEMENT_KEYWORD",              value: "for" },
-    { type: "IDENTIFIER",                     value: "i" },
-    { type: "STATEMENT_KEYWORD",              value: "in" },
-    { type: 'NUMBER',                         value: '0' },
-    { type: 'HALF-OPEN_RANGE',                value: '..<'},
-    { type: 'NUMBER',                         value: '5' },
-    { type: "PUNCTUATION",                    value: "{" },
-    { type: "TERMINATOR",                     value: "\\n"},
-    { type: "IDENTIFIER",                     value: "sum" },
-    { type: "OPERATOR",                       value: "+" },
-    { type: "OPERATOR",                       value: "=" },
-    { type: "IDENTIFIER",                     value: "i" },
-    { type: "TERMINATOR",                     value: "\\n"},
-    { type: "PUNCTUATION",                    value: "}" },
-    { type: "TERMINATOR",                     value: "EOF"},
-  ];
+  { type: "DECLARATION_KEYWORD",        value: "func"},
+  { type: "IDENTIFIER",                 value: "greet" },
+  { type: "PARAMS_START",               value: "(" },
+  { type: "IDENTIFIER",                 value: "name" },
+  { type: "PUNCTUATION",                value: ":" }, 
+  { type: "TYPE_STRING",                value: "String" }, 
+  { type: "PUNCTUATION",                value: "," },
+  { type: "IDENTIFIER",                 value: "day" },
+  { type: "PUNCTUATION",                value: ":" }, 
+  { type: "TYPE_STRING",                value: "String" }, 
+  { type: "PARAMS_END",                 value: ")" }, 
+  { type: "RETURN_ARROW",               value: "->" }, 
+  { type: "TYPE_STRING",                value: "String" }, 
+  { type: "STATEMENTS_START",           value: "{" },  
+  { type: "TERMINATOR",                 value: "\\n"},
+  { type: "STATEMENT_KEYWORD",          value: "return"},
+  { type: "STRING",                     value: "Hello " },
+  { type: "STRING_INTERPOLATION_START", value: "\\(" },
+  { type: "IDENTIFIER",                 value: "name" },
+  { type: "STRING_INTERPOLATION_END",   value: ")" },
+  { type: "STRING",                     value: ", today is " },
+  { type: "STRING_INTERPOLATION_START", value: "\\(" },
+  { type: "IDENTIFIER",                 value: "day" },
+  { type: "STRING_INTERPOLATION_END",   value: ")" },
+  { type: "STRING",                     value: "." },
+  { type: "TERMINATOR",                 value: "\\n"},
+  { type: "STATEMENTS_END",             value: "}" },
+  { type: "IDENTIFIER",                 value: "greet" },
+  { type: "INVOCATION_START",           value: "(" }, 
+  { type: "STRING",                     value: "Bob" },   
+  { type: "PUNCTUATION",                value: "," },
+  { type: "IDENTIFIER",                 value: "day" },
+  { type: "PUNCTUATION",                value: ":" },
+  { type: "STRING",                     value: "Tuesday" },   
+  { type: "INVOCATION_END",             value: ")" }, 
+  { type: "TERMINATOR",                 value: "EOF"}
+];
 
 
 console.log(lexer(swiftCode));
