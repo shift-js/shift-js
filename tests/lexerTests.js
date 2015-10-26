1e93f254f5c2e1daa70159f7659f48dd5907b551
Dnld <steinert.donald@gmail.com>
2015-10-26 17:12:23 -0400

    fixes test description

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -3179,7 +3179,7 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
-      xit('hould handle functions that do no use var when declaring parameters and invocations with named arguments', function () {
+      xit('should handle functions that do no use var when declaring parameters and invocations with named arguments', function () {
         input = String.raw`func greet(name: String, day: String) -> String {
                         return "Hello \(name), today is \(day)."
                     }
