faab959b05d70aab9c839c039bae2e7d2dea887c
Dnld <steinert.donald@gmail.com>
2015-10-27 18:18:50 -0400

    handles variable and constant stored properties

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -4323,6 +4323,53 @@ describe('Lexer', function() {
           expect(lexer(input)).to.deep.equal(output);
         });
 
+        it('should handle variable and constant stored properties', function () {
+          input = String.raw`struct FixedLengthRange {
+                                  var firstValue: Int
+                                  let length: Int
+                              }
+
+                              let rangeOfOneHundred = FixedLengthRange(firstValue: 1, length: 100)`;
+          output = [
+            { type: "DECLARATION_KEYWORD",        value: "struct" },
+            { type: "IDENTIFIER",                 value: "FixedLengthRange" },
+            { type: "STRUCT_DEFINITION_START",    value: "{" },
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "DECLARATION_KEYWORD",        value: "var" },
+            { type: "IDENTIFIER",                 value: "firstValue" },
+            { type: "PUNCTUATION",                value: ":" },
+            { type: "TYPE_NUMBER",                value: "Int"},
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "DECLARATION_KEYWORD",        value: "let" },
+            { type: "IDENTIFIER",                 value: "length" },
+            { type: "PUNCTUATION",                value: ":" },
+            { type: "TYPE_NUMBER",                value: "Int"},
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "STRUCT_DEFINITION_END",      value: "}" },
+            { type: "TERMINATOR",                 value: "\\n"},
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "DECLARATION_KEYWORD",        value: "let" },
+            { type: "IDENTIFIER",                 value: "rangeOfOneHundred" },
+            { type: "OPERATOR",                   value: "=" },
+            { type: "IDENTIFIER",                 value: "FixedLengthRange" },
+            { type: "INITIALIZATION_START",       value: "(" }, 
+            { type: "IDENTIFIER",                 value: "firstValue" },
+            { type: "PUNCTUATION",                value: ":" },
+            { type: "NUMBER",                     value: "1" },
+            { type: "PUNCTUATION",                value: "," },
+            { type: "IDENTIFIER",                 value: "length" },
+            { type: "PUNCTUATION",                value: ":" },
+            { type: "NUMBER",                     value: "100" },
+            { type: "INITIALIZATION_END",         value: ")" }, 
+            { type: "TERMINATOR",                 value: "EOF"}
+          ];
+          expect(lexer(input)).to.deep.equal(output);
+        });
+
       });
 
     }); 
--- transpiler/lexer/singleTest.js
+++ transpiler/lexer/singleTest.js
@@ -2,29 +2,29 @@ var lexer = require("./lexer");
 var deepEqual = require("./helperFunctions").deepEqual;
 var diff = require("./helperFunctions").diff;
 
-var swiftCode = String.raw`struct Resolution {
-                                var width = 0
-                                var height = 0
-                            }
+var swiftCode = String.raw`struct FixedLengthRange {
+                          var firstValue: Int
+                          let length: Int
+                      }
 
-                            let someResolution = Resolution(width: 640, height: 480)`;
+                      let rangeOfOneHundred = FixedLengthRange(firstValue: 1, length: 100)`;
      
 var swiftCodeAnswers = [
           { type: "DECLARATION_KEYWORD",        value: "struct" },
-          { type: "IDENTIFIER",                 value: "Resolution" },
+          { type: "IDENTIFIER",                 value: "FixedLengthRange" },
           { type: "STRUCT_DEFINITION_START",    value: "{" },
           { type: "TERMINATOR",                 value: "\\n"},
           
           { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "width" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "0" },
+          { type: "IDENTIFIER",                 value: "firstValue" },
+          { type: "PUNCTUATION",                value: ":" },
+          { type: "TYPE_NUMBER",                value: "Int"},
           { type: "TERMINATOR",                 value: "\\n"},
           
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "height" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "0" },
+          { type: "DECLARATION_KEYWORD",        value: "let" },
+          { type: "IDENTIFIER",                 value: "length" },
+          { type: "PUNCTUATION",                value: ":" },
+          { type: "TYPE_NUMBER",                value: "Int"},
           { type: "TERMINATOR",                 value: "\\n"},
           
           { type: "STRUCT_DEFINITION_END",      value: "}" },
@@ -32,17 +32,17 @@ var swiftCodeAnswers = [
           { type: "TERMINATOR",                 value: "\\n"},
           
           { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "someResolution" },
+          { type: "IDENTIFIER",                 value: "rangeOfOneHundred" },
           { type: "OPERATOR",                   value: "=" },
-          { type: "IDENTIFIER",                 value: "Resolution" },
+          { type: "IDENTIFIER",                 value: "FixedLengthRange" },
           { type: "INITIALIZATION_START",       value: "(" }, 
-          { type: "IDENTIFIER",                 value: "width" },
+          { type: "IDENTIFIER",                 value: "firstValue" },
           { type: "PUNCTUATION",                value: ":" },
-          { type: "NUMBER",                     value: "640" },
+          { type: "NUMBER",                     value: "1" },
           { type: "PUNCTUATION",                value: "," },
-          { type: "IDENTIFIER",                 value: "height" },
+          { type: "IDENTIFIER",                 value: "length" },
           { type: "PUNCTUATION",                value: ":" },
-          { type: "NUMBER",                     value: "480" },
+          { type: "NUMBER",                     value: "100" },
           { type: "INITIALIZATION_END",         value: ")" }, 
           { type: "TERMINATOR",                 value: "EOF"}
         ];
