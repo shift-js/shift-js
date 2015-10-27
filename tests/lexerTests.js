87624565478731303e5fd2b3b2f92f658dc4f4f2
Dnld <steinert.donald@gmail.com>
2015-10-26 21:32:59 -0400

    adds tests for functions that return functions

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -3651,6 +3651,108 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
+      xit('should handle functions that return functions where the return function is specified within parentheses', function () {
+        input = String.raw`func makeIncrementer() -> ((Int) -> Int) {
+                              func addOne(number: Int) -> Int {
+                                  return 1 + number
+                              }
+                              return addOne
+                          }`;
+        output = [
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "makeIncrementer" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "RETURN_ARROW",               value: "->" },
+          { type: "PUNCTUATION",                value: "(" }, 
+          { type: "PARAMS_START",               value: "(" },
+          { type: "TYPE_NUMBER",                value: "Int" },
+          { type: "PARAMS_END",                 value: ")" },
+          { type: "RETURN_ARROW",               value: "->" }, 
+          { type: "PUNCTUATION",                value: ")" },
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "addOne" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "IDENTIFIER",                 value: "number" },
+          { type: "PUNCTUATION",                value: ":" }, 
+          { type: "TYPE_NUMBER",                value: "Int" },
+          { type: "PARAMS_END",                 value: ")" },
+          { type: "RETURN_ARROW",               value: "->" },
+          { type: "TYPE_NUMBER",                value: "Int" },
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENT_KEYWORD",          value: "return"},
+          { type: "NUMBER",                     value: "1" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "IDENTIFIER",                 value: "number" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENT_KEYWORD",          value: "return"},
+          { type: "IDENTIFIER",                 value: "addOne" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
+      xit('should handle functions that return functions where the return function is specified without parentheses', function () {
+        input = String.raw`func makeIncrementer() -> (Int) -> Int {
+                              func addOne(number: Int) -> Int {
+                                  return 1 + number
+                              }
+                              return addOne
+                          }`;
+        output = [
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "makeIncrementer" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "RETURN_ARROW",               value: "->" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "TYPE_NUMBER",                value: "Int" },
+          { type: "PARAMS_END",                 value: ")" },
+          { type: "RETURN_ARROW",               value: "->" }, 
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "addOne" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "IDENTIFIER",                 value: "number" },
+          { type: "PUNCTUATION",                value: ":" }, 
+          { type: "TYPE_NUMBER",                value: "Int" },
+          { type: "PARAMS_END",                 value: ")" },
+          { type: "RETURN_ARROW",               value: "->" },
+          { type: "TYPE_NUMBER",                value: "Int" },
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENT_KEYWORD",          value: "return"},
+          { type: "NUMBER",                     value: "1" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "IDENTIFIER",                 value: "number" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENT_KEYWORD",          value: "return"},
+          { type: "IDENTIFIER",                 value: "addOne" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
     }); 
 
   });
