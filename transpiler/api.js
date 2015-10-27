3410adf25faa730390d804354b8ae2331ff96352
David Churchill <dchurchill92@gmail.com>
2015-10-27 13:11:22 -0500

    modified parser reference in api

--- transpiler/api.js
+++ transpiler/api.js
@@ -1,12 +1,22 @@
 var fs = require('fs');
 var lexer = require('./lexer/lexer.js');
-var parser = require('./parser/parser.js')();
+var make_parser = require('./parser/parser.js');
 var escodegen = require('escodegen');
 var generator = escodegen.generate;
+var parser;
+
+var options = {
+  format: {
+    indent: {
+      style: '  ',
+    }
+  }
+};
 
 module.exports = {
   compile: function(swiftString) {
-    return generator(parser(lexer(swiftString)));
+    parser = make_parser();
+    return generator(parser(lexer(swiftString)), options);
   },
   tokenize: function(swiftString) {
     return lexer(swiftString);
