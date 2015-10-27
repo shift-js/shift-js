fc94ec2203980424647ebc362b2bb9cd433c44fb
Dnld <steinert.donald@gmail.com>
2015-10-27 19:33:33 -0400

    handles complex memberwise initialization with internal parentheses

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -4325,6 +4325,72 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
+      it('should handle complex memberwise initialization with internal parentheses', function () {
+        input = String.raw`var resolutionHeight = 480
+                            struct Resolution {
+                                var width = 0
+                                var height = 0
+                            }
+
+                            let someResolution = Resolution(width: ((50 * 2) * 6) + 40, height: resolutionHeight)`;
+        output = [
+          { type: "DECLARATION_KEYWORD",        value: "var" },
+          { type: "IDENTIFIER",                 value: "resolutionHeight" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "480" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "struct" },
+          { type: "IDENTIFIER",                 value: "Resolution" },
+          { type: "STRUCT_DEFINITION_START",    value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "var" },
+          { type: "IDENTIFIER",                 value: "width" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "0" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "var" },
+          { type: "IDENTIFIER",                 value: "height" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "0" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STRUCT_DEFINITION_END",      value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "let" },
+          { type: "IDENTIFIER",                 value: "someResolution" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "IDENTIFIER",                 value: "Resolution" },
+          { type: "INITIALIZATION_START",       value: "(" }, 
+          { type: "IDENTIFIER",                 value: "width" },
+          { type: "PUNCTUATION",                value: ":" },
+          
+          { type: "PUNCTUATION",                value: "(" },
+          { type: "PUNCTUATION",                value: "(" },
+          { type: "NUMBER",                     value: "50" },
+          { type: "OPERATOR",                   value: "*" },
+          { type: "NUMBER",                     value: "2" },
+          { type: "PUNCTUATION",                value: ")" },
+          { type: "OPERATOR",                   value: "*" },
+          { type: "NUMBER",                     value: "6" },
+          { type: "PUNCTUATION",                value: ")" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "NUMBER",                     value: "40" },
+
+          { type: "PUNCTUATION",                value: "," },
+          { type: "IDENTIFIER",                 value: "height" },
+          { type: "PUNCTUATION",                value: ":" },
+          { type: "IDENTIFIER",                 value: "resolutionHeight" },
+          { type: "INITIALIZATION_END",         value: ")" }, 
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
       it('should handle variable and constant stored properties', function () {
         input = String.raw`struct FixedLengthRange {
                                 var firstValue: Int
--- transpiler/lexer/singleTest.js
+++ transpiler/lexer/singleTest.js
@@ -2,145 +2,68 @@ var lexer = require("./lexer");
 var deepEqual = require("./helperFunctions").deepEqual;
 var diff = require("./helperFunctions").diff;
 
-var swiftCode = String.raw`                                     class Medley {
-                  var a                                    = 1
-                  var b = "hai, world"
-                  let c =   true
-                  /* Comment 1 
-                  
-                  */ var                                  d = 1 // Comment 2
-              var e = [                    "Eggs", "Milk" ,"Bacon" ];
-                  var f = ["one": 1, "two": 2, "three": 3]
-                  let http200Status = (statusCode: 200,             description: "OK")
-                  var  g = 5 + 6 /          4 - (-16   % 4.2)*55
-                  let h = 6 != 9
-                  var           i = "Stephen"      + " " + "Tabor" +              "!"
-              }`;
+var swiftCode = String.raw`var resolutionHeight = 480
+                        struct Resolution {
+                            var width = 0
+                            var height = 0
+                        }
+
+                        let someResolution = Resolution(width: ((50 * 2) * 6) + 40, height: resolutionHeight)`;
      
 var swiftCodeAnswers = [
-          { type: "DECLARATION_KEYWORD",        value: "class" },
-          { type: "IDENTIFIER",                 value: "Medley" },
-          { type: "CLASS_DEFINITION_START",     value: "{" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "a" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "1" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "b" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "STRING",                     value: "hai, world" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "c" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "BOOLEAN",                    value: "true" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "MULTI_LINE_COMMENT_START",   value: "/*"},
-          { type: "COMMENT",                    value: " Comment 1 "},
-          { type: "TERMINATOR",                 value: "\\n"},
-          { type: "TERMINATOR",                 value: "\\n"},
-          { type: "MULTI_LINE_COMMENT_END",     value: "*/"},
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "d" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "1" },
-          { type: "COMMENT_START",              value: "//"},
-          { type: "COMMENT",                    value: " Comment 2"},
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "e" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "ARRAY_START",                value: "[" },
-          { type: "STRING",                     value: "Eggs" },
-          { type: "PUNCTUATION",                value: "," },
-          { type: "STRING",                     value: "Milk" },
-          { type: "PUNCTUATION",                value: "," },
-          { type: "STRING",                     value: "Bacon" },
-          { type: "ARRAY_END",                  value: "]" },
-          { type: "PUNCTUATION",                value: ";" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "f" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "DICTIONARY_START",           value: "[" },
-          { type: "STRING",                     value: "one" },
-          { type: "PUNCTUATION",                value: ":" },
-          { type: "NUMBER",                     value: "1" },
-          { type: "PUNCTUATION",                value: "," },
-          { type: "STRING",                     value: "two" },
-          { type: "PUNCTUATION",                value: ":" },
-          { type: "NUMBER",                     value: "2" },
-          { type: "PUNCTUATION",                value: "," },
-          { type: "STRING",                     value: "three" },
-          { type: "PUNCTUATION",                value: ":" },
-          { type: "NUMBER",                     value: "3" },
-          { type: "DICTIONARY_END",             value: "]" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "http200Status" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "TUPLE_START",                value: "("},
-          { type: "TUPLE_ELEMENT_NAME",         value: "statusCode"},
-          { type: "PUNCTUATION",                value: ":" },
-          { type: "NUMBER",                     value: "200"},
-          { type: "PUNCTUATION",                value: "," },
-          { type: "TUPLE_ELEMENT_NAME",         value: "description"},
-          { type: "PUNCTUATION",                value: ":" },
-          { type: "STRING",                     value: "OK"},
-          { type: "TUPLE_END",                  value: ")"},
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "g" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "5" },
-          { type: "OPERATOR",                   value: "+" },
-          { type: "NUMBER",                     value: "6" },
-          { type: "OPERATOR",                   value: "/" },
-          { type: "NUMBER",                     value: "4" },
-          { type: "OPERATOR",                   value: "-" },
-          { type: "PUNCTUATION",                value: "(" },
-          { type: "OPERATOR",                   value: "-" },
-          { type: "NUMBER",                     value: "16" },
-          { type: "OPERATOR",                   value: "%" },
-          { type: "NUMBER",                     value: "4.2" },
-          { type: "PUNCTUATION",                value: ")" },
-          { type: "OPERATOR",                   value: "*" },
-          { type: "NUMBER",                     value: "55" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "h" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "6" },
-          { type: "OPERATOR",                   value: "!" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "9" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "i" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "STRING",                     value: "Stephen" },
-          { type: "OPERATOR",                   value: "+" },
-          { type: "STRING",                     value: " " },
-          { type: "OPERATOR",                   value: "+" },
-          { type: "STRING",                     value: "Tabor" },
-          { type: "OPERATOR",                   value: "+" },
-          { type: "STRING",                     value: "!" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "CLASS_DEFINITION_END",       value: "}" },
-          { type: "TERMINATOR",                 value: "EOF"}
+         { type: "DECLARATION_KEYWORD",        value: "var" },
+         { type: "IDENTIFIER",                 value: "resolutionHeight" },
+         { type: "OPERATOR",                   value: "=" },
+         { type: "NUMBER",                     value: "480" },
+         { type: "TERMINATOR",                 value: "\\n"},
+         
+         { type: "DECLARATION_KEYWORD",        value: "struct" },
+         { type: "IDENTIFIER",                 value: "Resolution" },
+         { type: "STRUCT_DEFINITION_START",    value: "{" },
+         { type: "TERMINATOR",                 value: "\\n"},
+         
+         { type: "DECLARATION_KEYWORD",        value: "var" },
+         { type: "IDENTIFIER",                 value: "width" },
+         { type: "OPERATOR",                   value: "=" },
+         { type: "NUMBER",                     value: "0" },
+         { type: "TERMINATOR",                 value: "\\n"},
+         
+         { type: "DECLARATION_KEYWORD",        value: "var" },
+         { type: "IDENTIFIER",                 value: "height" },
+         { type: "OPERATOR",                   value: "=" },
+         { type: "NUMBER",                     value: "0" },
+         { type: "TERMINATOR",                 value: "\\n"},
+         
+         { type: "STRUCT_DEFINITION_END",      value: "}" },
+         { type: "TERMINATOR",                 value: "\\n"},
+         { type: "TERMINATOR",                 value: "\\n"},
+         
+         { type: "DECLARATION_KEYWORD",        value: "let" },
+         { type: "IDENTIFIER",                 value: "someResolution" },
+         { type: "OPERATOR",                   value: "=" },
+         { type: "IDENTIFIER",                 value: "Resolution" },
+         { type: "INITIALIZATION_START",       value: "(" }, 
+         { type: "IDENTIFIER",                 value: "width" },
+         { type: "PUNCTUATION",                value: ":" },
+         
+         { type: "PUNCTUATION",                value: "(" },
+         { type: "PUNCTUATION",                value: "(" },
+         { type: "NUMBER",                     value: "50" },
+         { type: "OPERATOR",                   value: "*" },
+         { type: "NUMBER",                     value: "2" },
+         { type: "PUNCTUATION",                value: ")" },
+         { type: "OPERATOR",                   value: "*" },
+         { type: "NUMBER",                     value: "6" },
+         { type: "PUNCTUATION",                value: ")" },
+         { type: "OPERATOR",                   value: "+" },
+         { type: "NUMBER",                     value: "40" },
+
+         { type: "PUNCTUATION",                value: "," },
+         { type: "IDENTIFIER",                 value: "height" },
+         { type: "PUNCTUATION",                value: ":" },
+         { type: "IDENTIFIER",                 value: "resolutionHeight" },
+         { type: "INITIALIZATION_END",         value: ")" }, 
+         { type: "TERMINATOR",                 value: "EOF"}
         ];
 
 console.log(lexer(swiftCode));
