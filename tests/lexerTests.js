e7b295e26307d82ac1b3471b5355041ee2d44058
Dnld <steinert.donald@gmail.com>
2015-10-27 11:25:24 -0400

    adds test for ranges delimited by identifiers

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -749,6 +749,39 @@ describe('Lexer', function() {
         ];
         expect(lexer(input)).to.deep.equal(output);
       });
+      
+      it('should handle ranges delimited by identifiers', function () {
+        input = String.raw`let start = 0; let end = 10; let range = start..<end; let fullRange = start...end;`;
+        output = [
+          { type: "DECLARATION_KEYWORD",            value: "let" },
+          { type: "IDENTIFIER",                 value: "start" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "0" },
+          { type: "PUNCTUATION",                value: ";" },
+          { type: "DECLARATION_KEYWORD",        value: "let" },
+          { type: "IDENTIFIER",                 value: "end" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "10" },
+          { type: "PUNCTUATION",                value: ";" },
+          { type: "DECLARATION_KEYWORD",        value: "let" },
+          { type: "IDENTIFIER",                 value: "range" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "IDENTIFIER",                 value: "start" },
+          { type: "HALF-OPEN_RANGE",            value: "..<" },
+          { type: "IDENTIFIER",                 value: "end" },
+          { type: "PUNCTUATION",                value: ";" },
+          { type: "DECLARATION_KEYWORD",        value: "let" },
+          { type: "IDENTIFIER",                 value: "fullRange" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "IDENTIFIER",                 value: "start" },
+          { type: "CLOSED_RANGE",               value: "..." },
+          { type: "IDENTIFIER",                 value: "end" },
+          { type: "PUNCTUATION",                value: ";" },
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+      
     });
 
     describe('String concatenation and interpolation', function () {
