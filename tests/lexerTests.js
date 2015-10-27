8f0d4b07005217d11c9b160da69da17a367f5f9a
Dnld <steinert.donald@gmail.com>
2015-10-27 17:50:45 -0400

    handles memberwise initialization

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -4276,6 +4276,53 @@ describe('Lexer', function() {
           expect(lexer(input)).to.deep.equal(output);
         });
 
+        it('should handle basic memberwise initialization', function () {
+          input = String.raw`struct Resolution {
+                                var width = 0
+                                var height = 0
+                            }
+
+                            let someResolution = Resolution(width: 640, height: 480)`;
+          output = [
+            { type: "DECLARATION_KEYWORD",        value: "struct" },
+            { type: "IDENTIFIER",                 value: "Resolution" },
+            { type: "STRUCT_DEFINITION_START",    value: "{" },
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "DECLARATION_KEYWORD",        value: "var" },
+            { type: "IDENTIFIER",                 value: "width" },
+            { type: "OPERATOR",                   value: "=" },
+            { type: "NUMBER",                     value: "0" },
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "DECLARATION_KEYWORD",        value: "var" },
+            { type: "IDENTIFIER",                 value: "height" },
+            { type: "OPERATOR",                   value: "=" },
+            { type: "NUMBER",                     value: "0" },
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "STRUCT_DEFINITION_END",      value: "}" },
+            { type: "TERMINATOR",                 value: "\\n"},
+            { type: "TERMINATOR",                 value: "\\n"},
+            
+            { type: "DECLARATION_KEYWORD",        value: "let" },
+            { type: "IDENTIFIER",                 value: "someResolution" },
+            { type: "OPERATOR",                   value: "=" },
+            { type: "IDENTIFIER",                 value: "Resolution" },
+            { type: "INITIALIZATION_START",       value: "(" }, 
+            { type: "IDENTIFIER",                 value: "width" },
+            { type: "PUNCTUATION",                value: ":" },
+            { type: "NUMBER",                     value: "640" },
+            { type: "PUNCTUATION",                value: "," },
+            { type: "IDENTIFIER",                 value: "height" },
+            { type: "PUNCTUATION",                value: ":" },
+            { type: "NUMBER",                     value: "480" },
+            { type: "INITIALIZATION_END",         value: ")" }, 
+            { type: "TERMINATOR",                 value: "EOF"}
+          ];
+          expect(lexer(input)).to.deep.equal(output);
+        });
+
       });
 
     }); 
--- transpiler/lexer/singleTest.js
+++ transpiler/lexer/singleTest.js
@@ -2,42 +2,14 @@ var lexer = require("./lexer");
 var deepEqual = require("./helperFunctions").deepEqual;
 var diff = require("./helperFunctions").diff;
 
-var swiftCode = String.raw`class VideoMode {
-                                 var interlaced = false
-                                 var frameRate = 0.0
-                              }
-                              struct Resolution {
-                                  var width = 0
-                                  var height = 0
-                              }
+var swiftCode = String.raw`struct Resolution {
+                                var width = 0
+                                var height = 0
+                            }
 
-                              let someVideoMode = VideoMode()
-                              let someResolution = Resolution();
-                              
-                              let someFrameRate = someVideoMode.frameRate;
-                              let someWidth = someResolution.width`;
+                            let someResolution = Resolution(width: 640, height: 480)`;
      
 var swiftCodeAnswers = [
-          { type: "DECLARATION_KEYWORD",        value: "class" },
-          { type: "IDENTIFIER",                 value: "VideoMode" },
-          { type: "CLASS_DEFINITION_START",     value: "{" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "interlaced" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "BOOLEAN",                    value: "false" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "var" },
-          { type: "IDENTIFIER",                 value: "frameRate" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "NUMBER",                     value: "0.0" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "CLASS_DEFINITION_END",       value: "}" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
           { type: "DECLARATION_KEYWORD",        value: "struct" },
           { type: "IDENTIFIER",                 value: "Resolution" },
           { type: "STRUCT_DEFINITION_START",    value: "{" },
@@ -58,40 +30,20 @@ var swiftCodeAnswers = [
           { type: "STRUCT_DEFINITION_END",      value: "}" },
           { type: "TERMINATOR",                 value: "\\n"},
           { type: "TERMINATOR",                 value: "\\n"},
-
-          { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "someVideoMode" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "IDENTIFIER",                 value: "VideoMode" },
-          { type: "INSTANTIATION_START",        value: "(" }, 
-          { type: "INSTANTIATION_END",          value: ")" }, 
-          { type: "TERMINATOR",                 value: "\\n"}, 
-
+          
           { type: "DECLARATION_KEYWORD",        value: "let" },
           { type: "IDENTIFIER",                 value: "someResolution" },
           { type: "OPERATOR",                   value: "=" },
           { type: "IDENTIFIER",                 value: "Resolution" },
-          { type: "INSTANTIATION_START",        value: "(" }, 
-          { type: "INSTANTIATION_END",          value: ")" }, 
-          { type: "PUNCTUATION",                value: ";" },
-          { type: "TERMINATOR",                 value: "\\n"}, 
-          { type: "TERMINATOR",                 value: "\\n"}, 
-          
-          { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "someFrameRate" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "IDENTIFIER",                 value: "someVideoMode" },
-          { type: "DOT_SYNTAX",                 value: "." },
-          { type: "IDENTIFIER",                 value: "frameRate" },
-          { type: "PUNCTUATION",                value: ";" },
-          { type: "TERMINATOR",                 value: "\\n"},
-          
-          { type: "DECLARATION_KEYWORD",        value: "let" },
-          { type: "IDENTIFIER",                 value: "someWidth" },
-          { type: "OPERATOR",                   value: "=" },
-          { type: "IDENTIFIER",                 value: "someResolution" },
-          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "INITIALIZATION_START",       value: "(" }, 
           { type: "IDENTIFIER",                 value: "width" },
+          { type: "PUNCTUATION",                value: ":" },
+          { type: "NUMBER",                     value: "640" },
+          { type: "PUNCTUATION",                value: "," },
+          { type: "IDENTIFIER",                 value: "height" },
+          { type: "PUNCTUATION",                value: ":" },
+          { type: "NUMBER",                     value: "480" },
+          { type: "INITIALIZATION_END",         value: ")" }, 
           { type: "TERMINATOR",                 value: "EOF"}
         ];
 
