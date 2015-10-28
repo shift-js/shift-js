afde65c9bf960164a099c49acabc8c81fbb17d7b
Dnld <steinert.donald@gmail.com>
2015-10-28 10:25:27 -0400

    handles instance methods that use self

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -4631,7 +4631,7 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
-      it('should handle basic instance method definitions and their invocation', function () {
+      it('should handle basic class instance method definitions, and their invocation', function () {
         input = String.raw`class Counter {
                                 var count = 0
                                 func increment() {
@@ -4730,7 +4730,7 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
-      it('should handle basic instance method definitions with multiple parameter names and their invocation', function () {
+      it('should handle basic class instance method definitions with multiple parameter names, and their invocation', function () {
         input = String.raw`class Counter {
                                 var count = 0
                                 func increment() {
@@ -4839,6 +4839,111 @@ describe('Lexer', function() {
           { type: "IDENTIFIER",                 value: "someCounter" },
           { type: "DOT_SYNTAX",                 value: "." },
           { type: "IDENTIFIER",                 value: "count" },
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
+      it('should handle basic instance method definitions that use self, and their invocation', function () {
+        input = String.raw`class Counter {
+                                var count = 0
+                                func increment() {
+                                    ++self.count
+                                }
+                                func incrementBy(amount: Int) {
+                                    self.count += amount
+                                }
+                                func reset() {
+                                    self.count = 0
+                                }
+                            }
+                            var someCounter = Counter()
+                            someCounter.incrementBy(5)`;
+
+        output = [
+          { type: "DECLARATION_KEYWORD",        value: "class" },
+          { type: "IDENTIFIER",                 value: "Counter" },
+          { type: "CLASS_DEFINITION_START",     value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "DECLARATION_KEYWORD",        value: "var" },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "0" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "increment" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "OPERATOR",                   value: "+" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "incrementBy" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "IDENTIFIER",                 value: "amount" },
+          { type: "PUNCTUATION",                value: ":" }, 
+          { type: "TYPE_NUMBER",                value: "Int" },  
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "IDENTIFIER",                 value: "amount" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "reset" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "0" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "CLASS_DEFINITION_END",       value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          {type: "DECLARATION_KEYWORD",         value: "var" },
+          { type: "IDENTIFIER",                 value: "someCounter" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "IDENTIFIER",                 value: "Counter" },
+          { type: "INITIALIZATION_START",       value: "(" }, 
+          { type: "INITIALIZATION_END",         value: ")" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "IDENTIFIER",                 value: "someCounter" },
+          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "IDENTIFIER",                 value: "incrementBy" },
+          { type: "INVOCATION_START",           value: "(" }, 
+          { type: "NUMBER",                     value: "5" },
+          { type: "INVOCATION_END",             value: ")" }, 
           { type: "TERMINATOR",                 value: "EOF"}
         ];
         expect(lexer(input)).to.deep.equal(output);
--- transpiler/lexer/singleTest.js
+++ transpiler/lexer/singleTest.js
@@ -5,89 +5,88 @@ var diff = require("./helperFunctions").diff;
 var swiftCode = String.raw`class Counter {
                                 var count = 0
                                 func increment() {
-                                    ++count
+                                    ++self.count
                                 }
-                                func incrementBy(amount: Int, numberOfTimes: Int) {
-                                        count += amount * numberOfTimes
+                                func incrementBy(amount: Int) {
+                                    self.count += amount
                                 }
                                 func reset() {
-                                    count = 0
+                                    self.count = 0
                                 }
                             }
                             var someCounter = Counter()
-                            someCounter.incrementBy(50, numberOfTimes: 10)
-                            someCounter.count`;
+                            someCounter.incrementBy(5)`;
      
 var swiftCodeAnswers = [
          { type: "DECLARATION_KEYWORD",        value: "class" },
          { type: "IDENTIFIER",                 value: "Counter" },
          { type: "CLASS_DEFINITION_START",     value: "{" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "DECLARATION_KEYWORD",        value: "var" },
          { type: "IDENTIFIER",                 value: "count" },
          { type: "OPERATOR",                   value: "=" },
          { type: "NUMBER",                     value: "0" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "DECLARATION_KEYWORD",        value: "func"},
          { type: "IDENTIFIER",                 value: "increment" },
          { type: "PARAMS_START",               value: "(" },
          { type: "PARAMS_END",                 value: ")" }, 
          { type: "STATEMENTS_START",           value: "{" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "OPERATOR",                   value: "+" },
          { type: "OPERATOR",                   value: "+" },
+         { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+         { type: "DOT_SYNTAX",                 value: "." },
          { type: "IDENTIFIER",                 value: "count" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "STATEMENTS_END",             value: "}" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "DECLARATION_KEYWORD",        value: "func"},
          { type: "IDENTIFIER",                 value: "incrementBy" },
          { type: "PARAMS_START",               value: "(" },
          { type: "IDENTIFIER",                 value: "amount" },
          { type: "PUNCTUATION",                value: ":" }, 
          { type: "TYPE_NUMBER",                value: "Int" },  
-         { type: "PUNCTUATION",                value: "," },
-         { type: "IDENTIFIER",                 value: "numberOfTimes" },
-         { type: "PUNCTUATION",                value: ":" }, 
-         { type: "TYPE_NUMBER",                value: "Int" },  
          { type: "PARAMS_END",                 value: ")" }, 
          { type: "STATEMENTS_START",           value: "{" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
+         { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+         { type: "DOT_SYNTAX",                 value: "." },
          { type: "IDENTIFIER",                 value: "count" },
          { type: "OPERATOR",                   value: "+" },
          { type: "OPERATOR",                   value: "=" },
          { type: "IDENTIFIER",                 value: "amount" },
-         { type: "OPERATOR",                   value: "*" },
-         { type: "IDENTIFIER",                 value: "numberOfTimes" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "STATEMENTS_END",             value: "}" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "DECLARATION_KEYWORD",        value: "func"},
          { type: "IDENTIFIER",                 value: "reset" },
          { type: "PARAMS_START",               value: "(" },
          { type: "PARAMS_END",                 value: ")" }, 
          { type: "STATEMENTS_START",           value: "{" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
+         { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+         { type: "DOT_SYNTAX",                 value: "." },
          { type: "IDENTIFIER",                 value: "count" },
          { type: "OPERATOR",                   value: "=" },
          { type: "NUMBER",                     value: "0" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "STATEMENTS_END",             value: "}" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          { type: "CLASS_DEFINITION_END",       value: "}" },
          { type: "TERMINATOR",                 value: "\\n"},
-         
+
          {type: "DECLARATION_KEYWORD",         value: "var" },
          { type: "IDENTIFIER",                 value: "someCounter" },
          { type: "OPERATOR",                   value: "=" },
@@ -100,17 +99,8 @@ var swiftCodeAnswers = [
          { type: "DOT_SYNTAX",                 value: "." },
          { type: "IDENTIFIER",                 value: "incrementBy" },
          { type: "INVOCATION_START",           value: "(" }, 
-         { type: "NUMBER",                     value: "50" },
-         { type: "PUNCTUATION",                value: "," },
-         { type: "IDENTIFIER",                 value: "numberOfTimes" },
-         { type: "PUNCTUATION",                value: ":" },
-         { type: "NUMBER",                     value: "10" },
+         { type: "NUMBER",                     value: "5" },
          { type: "INVOCATION_END",             value: ")" }, 
-         { type: "TERMINATOR",                 value: "\\n"},
-         
-         { type: "IDENTIFIER",                 value: "someCounter" },
-         { type: "DOT_SYNTAX",                 value: "." },
-         { type: "IDENTIFIER",                 value: "count" },
          { type: "TERMINATOR",                 value: "EOF"}
         ];
 
