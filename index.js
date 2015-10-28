7bc0442bc724797702fc9a3fcd841a1582b3d6cf
David Churchill <dchurchill92@gmail.com>
2015-10-27 19:09:26 -0500

    fix server ports for heroku

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
+var port = process.env.PORT || 8080;
 
-app.listen(8080);
-console.log('ShiftJS listening on port 8080');
+app.listen(port);
+console.log('ShiftJS listening on port ' + port);
 
 module.exports = app;
