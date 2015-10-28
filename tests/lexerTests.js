22270f8463b5c80c1de5babff8657957610ceb0a
Dnld <steinert.donald@gmail.com>
2015-10-28 10:53:32 -0400

    handles basic struct mutating methods that assign to self

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -4949,7 +4949,7 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
-      it('should handle basic struct method definitions that use the mutating keyword', function () {
+      it('should handle basic struct mutating method definitions', function () {
         input = String.raw`struct Counter {
                                 var count = 0
                                 mutating func increment() {
@@ -5021,6 +5021,54 @@ describe('Lexer', function() {
           { type: "IDENTIFIER",                 value: "count" },
           { type: "OPERATOR",                   value: "=" },
           { type: "NUMBER",                     value: "0" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STRUCT_DEFINITION_END",      value: "}" },
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
+      it('should handle basic struct mutating methods that assign to self', function () {
+        input = String.raw`struct Counter {
+                                var count = 0
+                                mutating func increment() {
+                                    self = Counter(count: ++count)
+                                }
+                            }`;
+        output = [
+          { type: "DECLARATION_KEYWORD",        value: "struct" },
+          { type: "IDENTIFIER",                 value: "Counter" },
+          { type: "STRUCT_DEFINITION_START",    value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "DECLARATION_KEYWORD",        value: "var" },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "NUMBER",                     value: "0" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "CONTEXT_SPECIFIC_KEYWORD",   value: "mutating"},
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "increment" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
+          { type: "OPERATOR",                   value: "=" },
+          { type: "IDENTIFIER",                 value: "Counter" },
+          { type: "INITIALIZATION_START",       value: "(" },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "PUNCTUATION",                value: ":" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "OPERATOR",                   value: "+" },
+          { type: "IDENTIFIER",                 value: "count" },
+          { type: "INITIALIZATION_END",         value: ")" },
           { type: "TERMINATOR",                 value: "\\n"},
 
           { type: "STATEMENTS_END",             value: "}" },
--- transpiler/lexer/singleTest.js
+++ transpiler/lexer/singleTest.js
@@ -5,13 +5,7 @@ var diff = require("./helperFunctions").diff;
 var swiftCode = String.raw`struct Counter {
                                 var count = 0
                                 mutating func increment() {
-                                    ++count
-                                }
-                                mutating func incrementBy(amount: Int) {
-                                    count += amount
-                                }
-                                mutating func reset() {
-                                    count = 0
+                                    self = Counter(count: ++count)
                                 }
                             }`;
      
@@ -35,45 +29,16 @@ var swiftCodeAnswers = [
     { type: "STATEMENTS_START",           value: "{" },
     { type: "TERMINATOR",                 value: "\\n"},
 
-    { type: "OPERATOR",                   value: "+" },
-    { type: "OPERATOR",                   value: "+" },
-    { type: "IDENTIFIER",                 value: "count" },
-    { type: "TERMINATOR",                 value: "\\n"},
-
-    { type: "STATEMENTS_END",             value: "}" },
-    { type: "TERMINATOR",                 value: "\\n"},
-
-    { type: "CONTEXT_SPECIFIC_KEYWORD",   value: "mutating"},
-    { type: "DECLARATION_KEYWORD",        value: "func"},
-    { type: "IDENTIFIER",                 value: "incrementBy" },
-    { type: "PARAMS_START",               value: "(" },
-    { type: "IDENTIFIER",                 value: "amount" },
-    { type: "PUNCTUATION",                value: ":" }, 
-    { type: "TYPE_NUMBER",                value: "Int" },  
-    { type: "PARAMS_END",                 value: ")" }, 
-    { type: "STATEMENTS_START",           value: "{" },
-    { type: "TERMINATOR",                 value: "\\n"},
-
-    { type: "IDENTIFIER",                 value: "count" },
-    { type: "OPERATOR",                   value: "+" },
+    { type: "EXPRESSION_OR_TYPE_KEYWORD", value: "self" },
     { type: "OPERATOR",                   value: "=" },
-    { type: "IDENTIFIER",                 value: "amount" },
-    { type: "TERMINATOR",                 value: "\\n"},
-
-    { type: "STATEMENTS_END",             value: "}" },
-    { type: "TERMINATOR",                 value: "\\n"},
-
-    { type: "CONTEXT_SPECIFIC_KEYWORD",   value: "mutating"},
-    { type: "DECLARATION_KEYWORD",        value: "func"},
-    { type: "IDENTIFIER",                 value: "reset" },
-    { type: "PARAMS_START",               value: "(" },
-    { type: "PARAMS_END",                 value: ")" }, 
-    { type: "STATEMENTS_START",           value: "{" },
-    { type: "TERMINATOR",                 value: "\\n"},
-
+    { type: "IDENTIFIER",                 value: "Counter" },
+    { type: "INITIALIZATION_START",       value: "(" },
     { type: "IDENTIFIER",                 value: "count" },
-    { type: "OPERATOR",                   value: "=" },
-    { type: "NUMBER",                     value: "0" },
+    { type: "PUNCTUATION",                value: ":" },
+    { type: "OPERATOR",                   value: "+" },
+    { type: "OPERATOR",                   value: "+" },
+    { type: "IDENTIFIER",                 value: "count" },
+    { type: "INITIALIZATION_END",         value: ")" },
     { type: "TERMINATOR",                 value: "\\n"},
 
     { type: "STATEMENTS_END",             value: "}" },
