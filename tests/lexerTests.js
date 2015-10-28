0c06efc19a785c15707e30187fbfd7a47f832e78
Dnld <steinert.donald@gmail.com>
2015-10-28 16:49:03 -0400

    adds tes to handle type methods declared with the static or class keyword

--- tests/lexerTests.js
+++ tests/lexerTests.js
@@ -5136,6 +5136,78 @@ describe('Lexer', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
+      it('should handle type methods declared with the static or class keyword', function () {
+        input = String.raw`class ParentClass {
+                                static func returnTen() -> Int {
+                                    return 10
+                                }
+                                class func returnString() -> String {
+                                    return "my string"
+                                }
+                            }
+                            ParentClass.returnTen()
+                            ParentClass.returnString()`;
+
+        output = [
+          { type: "DECLARATION_KEYWORD",        value: "class" },
+          { type: "IDENTIFIER",                 value: "ParentClass" },
+          { type: "CLASS_DEFINITION_START",     value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "static"},
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "returnTen" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "RETURN_ARROW",               value: "->" },
+          { type: "TYPE_NUMBER",                value: "Int" },  
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENT_KEYWORD",          value: "return"},
+          { type: "NUMBER",                     value: "10" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",        value: "class"},
+          { type: "DECLARATION_KEYWORD",        value: "func"},
+          { type: "IDENTIFIER",                 value: "returnString" },
+          { type: "PARAMS_START",               value: "(" },
+          { type: "PARAMS_END",                 value: ")" }, 
+          { type: "RETURN_ARROW",               value: "->" },
+          { type: "TYPE_STRING",                value: "String" },  
+          { type: "STATEMENTS_START",           value: "{" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "STATEMENT_KEYWORD",          value: "return"},
+          { type: "STRING",                     value: "my string" },
+          { type: "TERMINATOR",                 value: "\\n"},
+
+          { type: "STATEMENTS_END",             value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "CLASS_DEFINITION_END",       value: "}" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "IDENTIFIER",                 value: "ParentClass" },
+          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "IDENTIFIER",                 value: "returnTen" },
+          { type: "INVOCATION_START",           value: "(" },
+          { type: "INVOCATION_END",             value: ")" },
+          { type: "TERMINATOR",                 value: "\\n"},
+          
+          { type: "IDENTIFIER",                 value: "ParentClass" },
+          { type: "DOT_SYNTAX",                 value: "." },
+          { type: "IDENTIFIER",                 value: "returnString" },
+          { type: "INVOCATION_START",           value: "(" },
+          { type: "INVOCATION_END",             value: ")" },
+          { type: "TERMINATOR",                 value: "EOF"}
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
     }); 
 
   });
