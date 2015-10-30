a1c9f05c7c521825e113267cf24bf678062c6ffa
Dnld <steinert.donald@gmail.com>
2015-10-30 19:57:10 -0400

    handles drop first method, adds appropriate tests

--- tests/lexerTests/lexerTestsThirdMilestone.js
+++ tests/lexerTests/lexerTestsThirdMilestone.js
@@ -4829,6 +4829,51 @@ describe('Lexer: Third Milestone', function() {
         expect(lexer(input)).to.deep.equal(output);
       });
 
+      it('should handle the drop first method', function () {
+        input = String.raw `var arr = [1,2,3,4,5]
+                              var lessLast = arr.dropLast()
+                              var lessLastThree = arr.dropLast(3)`;
+        output = [
+          { type: "DECLARATION_KEYWORD",          value: "var" },
+          { type: "IDENTIFIER",                   value: "arr" },
+          { type: "OPERATOR",                     value: "=" },
+          { type: "ARRAY_START",                  value: "[" },
+          { type: "NUMBER",                       value: "1" },
+          { type: "PUNCTUATION",                  value: "," },
+          { type: "NUMBER",                       value: "2" },
+          { type: "PUNCTUATION",                  value: "," },
+          { type: "NUMBER",                       value: "3" },
+          { type: "PUNCTUATION",                  value: "," },
+          { type: "NUMBER",                       value: "4" },
+          { type: "PUNCTUATION",                  value: "," },
+          { type: "NUMBER",                       value: "5" },
+          { type: "ARRAY_END",                    value: "]" },
+          { type: "TERMINATOR",                   value: "\\n"},
+          
+          { type: "DECLARATION_KEYWORD",          value: "var" },
+          { type: "IDENTIFIER",                   value: "lessLast" },
+          { type: "OPERATOR",                     value: "=" },
+          { type: "IDENTIFIER",                   value: "arr" },
+          { type: "DOT_SYNTAX",                   value: "." },
+          { type: "NATIVE_METHOD",                value: "dropLast" },
+          { type: "INVOCATION_START",             value: "(" },
+          { type: "INVOCATION_END",               value: ")" },
+          { type: "TERMINATOR",                   value: "\\n"},
+
+          { type: "DECLARATION_KEYWORD",          value: "var" },
+          { type: "IDENTIFIER",                   value: "lessLastThree" },
+          { type: "OPERATOR",                     value: "=" },
+          { type: "IDENTIFIER",                   value: "arr" },
+          { type: "DOT_SYNTAX",                   value: "." },
+          { type: "NATIVE_METHOD",                value: "dropLast" },
+          { type: "INVOCATION_START",             value: "(" },
+          { type: "NUMBER",                       value: "3" },
+          { type: "INVOCATION_END",               value: ")" },       
+          { type: "TERMINATOR",                   value: "EOF" }   
+        ];
+        expect(lexer(input)).to.deep.equal(output);
+      });
+
     });
 
   });
--- transpiler/lexer/lexicalTypes.js
+++ transpiler/lexer/lexicalTypes.js
@@ -17,6 +17,7 @@ module.exports = {
     'contains': 'NATIVE_METHOD',
     'distanceTo': 'NATIVE_METHOD',
     'dropFirst': 'NATIVE_METHOD',
+    'dropLast': 'NATIVE_METHOD',
     'hasPrefix': 'NATIVE_METHOD',
     'hasSuffix': 'NATIVE_METHOD',
     'insert': 'NATIVE_METHOD',
