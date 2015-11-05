19c84d5792b9ceb7661cfb7a1613e7bf79753be1
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-11-04 17:40:13 -0700

    Parser updates

--- tests/parserTests/parserTestsSecondMilestone.js
+++ tests/parserTests/parserTestsSecondMilestone.js
@@ -5733,7 +5733,7 @@ describe('Parser: Second Milestone', function() {
 
      TODO BUG BUG tup.0 doesn't work but tup[0] does. Fix
      */
-    xit('should handle calls to print multiline', function() {
+    it('should handle calls to print multiline', function() {
       input = [
         { type: "DECLARATION_KEYWORD",        value: "var" },
         { type: "IDENTIFIER",                 value: "name" },
